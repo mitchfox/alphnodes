663dcbb37d0ce876d4a8915fa539d4a2c168bef3
cgi-bin <s@ono.re>
2024-05-25 09:10:22 +0200

    remove: copyright

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -6,11 +6,11 @@ export default function Footer() {
   return (
     <footer>
       <div className="px-4 sm:px-6">
-        <div className="max-w-3xl mx-auto">
-          <div className="text-center py-8">
+        <div className="mx-auto max-w-3xl">
+          <div className="py-8 text-center">
             <div className="text-sm text-gray-700 dark:text-gray-400">
               {/* open in a new link */}
-              <p>© Alephium World</p>
+              <p>Alephium World</p>
               <p>Powered by <a className="text-green-500" href="https://notrustverify.ch" target="_blank" rel="noopener noreferrer">No Trust Verify</a> & <a className="text-green-500" href="https://x.com/pushvalue" target="_blank" rel="noopener noreferrer">Push Value</a></p>
             </div>
           </div>
