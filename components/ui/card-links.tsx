bf3889c833bcf7fd4cfdbf3da78f7f398f17c165
mitchfox <mjfox99@gmail.com>
2024-05-26 21:20:06 +0100

    pushing stats latest

--- components/ui/card-links.tsx
+++ components/ui/card-links.tsx
@@ -1,12 +1,8 @@
 'use client'
 
-
 import React, { useState, useEffect } from 'react';
-// import Card from './card';
-import data from '../../app/api/fullnodes.json';
-
-
 import dynamic from 'next/dynamic';
+
 const Card = dynamic(() => import('./card'), { ssr: false });
 const HistoryChart = dynamic(() => import('./history-chart'), { ssr: false });
 
@@ -15,7 +11,6 @@ export default function CardLinks() {
   const [versions, setVersions] = useState<{ name: string, count: number }[]>([]);
   const [versionsOs, setOsVersions] = useState<{ name: string, count: number }[]>([]);
   const [versionsFullnode, setFullnodeVersions] = useState<{ name: string, count: number }[]>([]);
-
 
   const handleData = (data: any) => {
     const countryMap: { [key: string]: number } = {};
@@ -28,37 +23,42 @@ export default function CardLinks() {
       const version = item.clientVersion.split("/")[1];
       const os = item.clientVersion.split("/")[2];
 
-
       countryMap[item.country] = (countryMap[item.country] || 0) + 1;
       versionMap[version] = (versionMap[version] || 0) + 1;
       versionOsMap[os] = (versionOsMap[os] || 0) + 1;
       versionFullnodeMap[fullnode] = (versionFullnodeMap[fullnode] || 0) + 1;
     });
+
     const countries = Object.keys(countryMap).map((country) => ({
       name: country,
       count: countryMap[country]
     }));
     const versions = Object.keys(versionMap).map((version) => ({
       name: version,
-      count: versionMap[version]      
+      count: versionMap[version]
     }));
     const versionOs = Object.keys(versionOsMap).map((version) => ({
       name: version,
-      count: versionOsMap[version]      
+      count: versionOsMap[version]
     }));
     const versionFullnode = Object.keys(versionFullnodeMap).map((version) => ({
       name: version,
-      count: versionFullnodeMap[version]      
-    }))
+      count: versionFullnodeMap[version]
+    }));
     return { countries, versions, versionOs, versionFullnode };
   };
 
   useEffect(() => {
-    const { countries, versions, versionOs, versionFullnode } = handleData(data);
-    setCountries(countries);
-    setVersions(versions);
-    setOsVersions(versionOs);
-    setFullnodeVersions(versionFullnode);
+    fetch('https://map.alephium.notrustverify.ch/fullnodes')
+      .then(response => response.json())
+      .then(data => {
+        const { countries, versions, versionOs, versionFullnode } = handleData(data);
+        setCountries(countries);
+        setVersions(versions);
+        setOsVersions(versionOs);
+        setFullnodeVersions(versionFullnode);
+      })
+      .catch(error => console.error('Error fetching data:', error));
   }, []);
 
   const segments = [
@@ -79,7 +79,7 @@ export default function CardLinks() {
     },
     {
       id: '2',
-      title: 'OS type' ,
+      title: 'OS type',
       description: 'Count of nodes by OS.',
       comingSoon: true,
       data: versionsOs.map(c => c.count),
@@ -87,14 +87,13 @@ export default function CardLinks() {
     },
     {
       id: '3',
-      title: 'Implementation type' ,
+      title: 'Implementation type',
       description: 'Count of nodes by OS.',
       comingSoon: true,
       data: versionsFullnode.map(c => c.count),
       labels: versionsFullnode.map(c => c.name),
     },
   ];
-
 
   return (
     <div className="pt-12 pb-16 space-y-8 grow md:flex md:space-y-0 md:space-x-8 md:pt-16 md:pb-20">
@@ -108,12 +107,8 @@ export default function CardLinks() {
               <section>
                 {/* Cards */}
                 <div className="grid gap-10 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1">
-              
-              <HistoryChart />
-              </div>
-
-
-
+                  <HistoryChart />
+                </div>
                 <div className="grid gap-10 mt-10 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2">
                   {segments.map((item) => (
                     <Card key={item.id} item={item} />
