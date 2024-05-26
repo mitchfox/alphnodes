2f1717b574b803734ae5e94d6b0f6d63182187b5
mitchfox <mjfox99@gmail.com>
2024-05-26 12:54:41 +0100

    updated node data

--- app/(default)/nodes/node-table.tsx
+++ app/(default)/nodes/node-table.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import data from '../../api/fullnodes.json';
-import ReactCountryFlag from "react-country-flag"
+import ReactCountryFlag from "react-country-flag";
 
 interface Node {
     ip: string;
@@ -21,63 +20,74 @@ interface Node {
 
 export default function NodeTable() {
     const [nodeData, setNodeData] = useState<Node[]>([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
-        // Fetch the node data from the JSON file
-        setNodeData(data);
+        // Fetch data from the API
+        fetch('https://map.alephium.notrustverify.ch/fullnodes')
+            .then(response => response.json())
+            .then(data => {
+                const nodesData = data.map((node: Node) => {
+                    const [lat, lng] = node.location.split(',').map(Number);
+                    return {
+                        ...node,
+                        lat,
+                        lng,
+                        clientVersion: node.clientVersion.match(/v\d+\.\d+\.\d+/)?.[0] || node.clientVersion,
+                    };
+                });
+                setNodeData(nodesData);
+                setLoading(false);
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+                setLoading(false);
+            });
     }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <>
-
-
+            <p style={{ textAlign: 'center' }} className="text-sm text-slate-500 dark:text-neutral-400 mb-4">
+                Currently displaying <span className="text-sm text-green-500">{nodeData && nodeData.length}</span> Nodes out of 125. More nodes will be added soon.
+            </p>
             <div className="max-w-5xl px-4 sm:px-6 mx-auto pb-12" style={{ margin: 'auto', width: 'auto' }}>
+
                 <div className="relative overflow-x-auto shadow-md rounded-lg">
+
+
                     <table className="w-full text-sm text-left rtl:text-right text-neutral-500 dark:text-neutral-400">
                         <thead className="text-xs text-neutral-800 uppercase bg-slate-50 dark:bg-neutral-700 dark:text-neutral-300">
                             <tr>
-                                {/* <th scope="col" className="px-6 py-3">IP Address</th> */}
                                 <th scope="col" className="px-6 py-3">Version</th>
-                                {/* <th scope="col" className="px-6 py-3">Hostname</th> */}
                                 <th scope="col" className="px-6 py-3">City</th>
                                 <th scope="col" className="px-6 py-3">Region</th>
                                 <th scope="col" className="px-6 py-3">Country</th>
-                                {/* <th scope="col" className="px-6 py-3">Location</th> */}
-                                {/* <th scope="col" className="px-6 py-3">Organization</th> */}
-                                {/* <th scope="col" className="px-6 py-3">PostCode</th> */}
                                 <th scope="col" className="px-6 py-3">Timezone</th>
                                 <th scope="col" className="px-6 py-3">Updated At</th>
                                 <th scope="col" className="px-6 py-3">Synced</th>
                             </tr>
                         </thead>
                         <tbody>
-
-
-                            {nodeData.map((node, index) => {
-
-                                const version = node.clientVersion?.match(/v\d+\.\d+\.\d+/)?.[0];
-                                return (
-                                    <tr key={index} className="bg-white border-b dark:bg-neutral-800 dark:border-neutral-700 hover:bg-neutral-50 dark:hover:bg-neutral-600">
-                                        {/* <td className="px-6 py-4 text-neutral-600 dark:text-neutral-400">{node.ip}</td> */}
-                                        <td style={{ textTransform: 'capitalize' }} className="px-6 py-4 text-neutral-600 dark:text-neutral-400">{version}</td>
-                                        {/* <td className="px-6 py-4 text-neutral-600 dark:text-neutral-400">{node.hostname}</td> */}
-                                        <td className="px-6 py-4 text-neutral-600 dark:text-neutral-400">{node.city}</td>
-                                        <td className="px-6 py-4 text-neutral-600 dark:text-neutral-400">{node.region}</td>
-                                        <td style={{ textAlign: 'center' }} className="px-6 py-4 text-neutral-600 dark:text-neutral-400"><ReactCountryFlag countryCode={node.country} style={{ fontSize: '20px' }} /></td>
-                                        {/* <td className="px-6 py-4 text-neutral-600 dark:text-neutral-400">{node.location}</td> */}
-                                        {/* <td className="px-6 py-4 text-neutral-600 dark:text-neutral-400">{node.org}</td> */}
-                                        {/* <td className="px-6 py-4 text-neutral-600 dark:text-neutral-400">{node.postal}</td> */}
-                                        <td className="px-6 py-4 text-neutral-600 dark:text-neutral-400">{node.timezone}</td>
-                                        <td className="px-6 py-4 text-neutral-600 dark:text-neutral-400">{new Date(node.updatedAt).toLocaleString()}</td>
-                                        <td className="px-6 py-4 text-neutral-600 dark:text-neutral-400">{node.isSynced ? "Yes" : "No"}</td>
-                                    </tr>
-                                )
-
-                            })}
+                            {nodeData.map((node, index) => (
+                                <tr key={index} className="bg-white border-b dark:bg-neutral-800 dark:border-neutral-700 hover:bg-neutral-50 dark:hover:bg-neutral-600">
+                                    <td style={{ textTransform: 'capitalize' }} className="px-6 py-4 text-neutral-600 dark:text-neutral-400">{node.clientVersion}</td>
+                                    <td className="px-6 py-4 text-neutral-600 dark:text-neutral-400">{node.city}</td>
+                                    <td className="px-6 py-4 text-neutral-600 dark:text-neutral-400">{node.region}</td>
+                                    <td style={{ textAlign: 'center' }} className="px-6 py-4 text-neutral-600 dark:text-neutral-400"><ReactCountryFlag countryCode={node.country} style={{ fontSize: '20px' }} /></td>
+                                    <td className="px-6 py-4 text-neutral-600 dark:text-neutral-400">{node.timezone}</td>
+                                    <td className="px-6 py-4 text-neutral-600 dark:text-neutral-400">{new Date(node.updatedAt).toLocaleString()}</td>
+                                    <td className="px-6 py-4 text-neutral-600 dark:text-neutral-400">{node.isSynced ? "Yes" : "No"}</td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
             </div>
         </>
+
     );
 }
--- app/(default)/nodes/page.tsx
+++ app/(default)/nodes/page.tsx
@@ -1,8 +1,4 @@
-{
-  /*
-Note: This code includes an example of how to fetch data from an external JSON file that is hosted at https://raw.githubusercontent.com/cruip/cruip-dummy/main/waitlist-posts.json. To facilitate this, we've included a lib directory in the root which contains a function that can fetch the JSON content. Additionally, we've defined the Post types in the types.d.ts file located in the root.
-*/
-}
+
 
 import NodeTable from "./node-table";
 
